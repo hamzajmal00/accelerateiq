77b6d0f7845b64d318167b5ce327e6f5d41d9228
Hamza Ajmal <tempmanagebyhamza@gmail.com>
2024-02-13 12:20:41 +0500

    fixes: header show in dashboard

--- src/components/ManagmentDasboard/MangerDashboard.js
+++ src/components/ManagmentDasboard/MangerDashboard.js
@@ -1,71 +1,71 @@
-import React, { useState } from "react";
-import Dashboard from "../../pages/Dashboard";
-import Tab1 from "./Tabs/Tab1";
-import Tab2 from "./Tabs/Tab2";
-import Tab3 from "./Tabs/Tab3";
+import React, { useState } from 'react';
+import ManagerDashboard from '../../pages/CallerDashboard';
+import Tab1 from './Tabs/Tab1';
+import Tab2 from './Tabs/Tab2';
+import Tab3 from './Tabs/Tab3';
 
 const MangerDashboard = () => {
   const [tabIndex, setTabIndex] = useState(0);
 
   return (
-    <Dashboard>
-      {" "}
-      <div class="border-b border-gray-200">
+    <ManagerDashboard>
+      {' '}
+      <div class='border-b border-gray-200'>
         <nav
-          class="flex space-x-4 lg:space-x-8"
-          aria-label="Tabs"
-          role="tablist"
+          class='flex space-x-4 lg:space-x-8'
+          aria-label='Tabs'
+          role='tablist'
         >
           <button
-            type="button"
-            class={`${tabIndex==0 && "active"} inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
-            id="todays-stats-tab-btn"
-            data-hs-tab="#todays-stats-tab"
-            aria-controls="todays-stats-tab"
+            type='button'
+            class={`${
+              tabIndex == 0 && 'active'
+            } inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
+            id='todays-stats-tab-btn'
+            data-hs-tab='#todays-stats-tab'
+            aria-controls='todays-stats-tab'
             onClick={() => setTabIndex(0)}
-            role="tab"
+            role='tab'
           >
             Today's Stats
           </button>
           <button
-            type="button"
-            class={`${tabIndex==1 && "active"} inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
-            id="this-week-tab-btn"
-            data-hs-tab="#this-week-tab"
-            aria-controls="this-week-tab"
+            type='button'
+            class={`${
+              tabIndex == 1 && 'active'
+            } inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
+            id='this-week-tab-btn'
+            data-hs-tab='#this-week-tab'
+            aria-controls='this-week-tab'
             onClick={() => setTabIndex(1)}
-            role="tab"
+            role='tab'
           >
             This Week
           </button>
           <button
-            type="button"
-            class={`${tabIndex==2 && "active"} inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
-            id="this-month-tab-btn"
-            data-hs-tab="#this-month-tab"
-            aria-controls="this-month-tab"
+            type='button'
+            class={`${
+              tabIndex == 2 && 'active'
+            } inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 font-medium text-primary/40 transition hover:text-primary focus:text-secondary focus:outline-none disabled:pointer-events-none disabled:opacity-50 hs-tab-active:border-secondary hs-tab-active:text-secondary`}
+            id='this-month-tab-btn'
+            data-hs-tab='#this-month-tab'
+            aria-controls='this-month-tab'
             onClick={() => setTabIndex(2)}
-            role="tab"
+            role='tab'
           >
             This Month
           </button>
         </nav>
       </div>
       {/* <!-- Tab contents --> */}
-      <div class="mt-8 flex-1">
-        {tabIndex == 0 && (
-        <Tab1/>
-        )}
+      <div class='mt-8 flex-1'>
+        {tabIndex == 0 && <Tab1 />}
 
-        {tabIndex == 1 && (
-        <Tab2/>
-        )}
+        {tabIndex == 1 && <Tab2 />}
 
-        {tabIndex == 2 && (
-        <Tab3/>
-        )}
+        {tabIndex == 2 && <Tab3 />}
       </div>
-    </Dashboard>
+    </ManagerDashboard>
   );
 };
 
